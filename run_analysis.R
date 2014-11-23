bcbd7e1c362e01a126694ef88077d84eaa1d56c1
Edolas <edo.elstak@gmail.com>
2014-11-23 19:37:03 +0100

    run analysis added

---
+++ run_analysis.R
@@ -0,0 +1,41 @@
+##load required libraries
+library(dplyr)
+library(tidyr)
+
+## load test data into variables
+test <- read.table("UCI HAR Dataset/test/X_test.txt")
+testsubj <- read.table("UCI HAR Dataset/test/subject_test.txt")
+testy <- read.table("UCI HAR Dataset/test/y_test.txt")
+feat <- read.table("UCI HAR Dataset/features.txt")
+feat2 <- make.names(feat[[2]], unique=TRUE)
+act_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+testy = join(testy, act_labels)
+
+## Add feature names
+colnames(test) <- feat2
+
+## Add subject and activity id to test data
+test$activity <- testy[[2]]
+test$subject <- testsubj[[1]]
+
+## load train data into variables
+train <- read.table("UCI HAR Dataset/train/X_train.txt")
+trainsubj <- read.table("UCI HAR Dataset/train/subject_train.txt")
+trainy <- read.table("UCI HAR Dataset/train/y_train.txt")
+trainy = join(trainy, act_labels)
+
+## Add feature names and save activiy names
+colnames(train) <- feat2
+
+## Add subject and activity id to test data
+train$activity <- trainy[[2]]
+train$subject <- trainsubj[[1]]
+
+## Merge data and load into df for use of dplyr
+## extract only mean and stdev data columns
+## create data set with average of each variable and subject
+AllData <- rbind(train,test)
+DF <- tbl_df(AllData)
+DF2 <- select(DF,subject, activity,contains(".std"),contains(".mean."))
+by_DF <- group_by(DF2,subject,activity)
+summarise_each(by_DF,funs(mean))
