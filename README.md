7e83d69e996408202e96fc6494a6014802638d7f
Edolas <edo.elstak@gmail.com>
2014-11-23 20:17:20 +0100

    read me and analysis updated

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ Get_data
 ========
 
 Getting and cleaning data - Coursera
+
+#README for run_analysis.R
+The R script does the following steps provided the unpacked data is present in the working directory.
+
+*load required libraries
+*load test data into variables
+*Load measurement names and extract unique names
+*Add descriptive names to activities
+*Add measurement names to test data
+*Add subject and activity id to test data
+*load train data into variables and add descriptive names
+*Add measurement names to train data
+*Add subject and activity id to train data
+*Merge data and load into df for use of dplyr
+*extract only mean and stdev data columns
+*create data set with average of each variable per subject and activity
+
+The script is annotated inside the code as well for clarity
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
 ##load required libraries
+library(plyr)
 library(dplyr)
 library(tidyr)
 
@@ -6,34 +7,38 @@ library(tidyr)
 test <- read.table("UCI HAR Dataset/test/X_test.txt")
 testsubj <- read.table("UCI HAR Dataset/test/subject_test.txt")
 testy <- read.table("UCI HAR Dataset/test/y_test.txt")
+
+## Load measurement names and extract unique names
 feat <- read.table("UCI HAR Dataset/features.txt")
 feat2 <- make.names(feat[[2]], unique=TRUE)
+
+## Add descriptive names to activities
 act_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 testy = join(testy, act_labels)
 
-## Add feature names
+## Add measurement names to test data
 colnames(test) <- feat2
 
 ## Add subject and activity id to test data
 test$activity <- testy[[2]]
 test$subject <- testsubj[[1]]
 
-## load train data into variables
+## load train data into variables and add descriptive names
 train <- read.table("UCI HAR Dataset/train/X_train.txt")
 trainsubj <- read.table("UCI HAR Dataset/train/subject_train.txt")
 trainy <- read.table("UCI HAR Dataset/train/y_train.txt")
 trainy = join(trainy, act_labels)
 
-## Add feature names and save activiy names
+## Add measurement names to train data
 colnames(train) <- feat2
 
-## Add subject and activity id to test data
+## Add subject and activity id to train data
 train$activity <- trainy[[2]]
 train$subject <- trainsubj[[1]]
 
 ## Merge data and load into df for use of dplyr
 ## extract only mean and stdev data columns
-## create data set with average of each variable and subject
+## create data set with average of each variable per subject and activity
 AllData <- rbind(train,test)
 DF <- tbl_df(AllData)
 DF2 <- select(DF,subject, activity,contains(".std"),contains(".mean."))
